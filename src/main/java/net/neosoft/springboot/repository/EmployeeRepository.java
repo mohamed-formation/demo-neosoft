70b11effb65e95f1566bf377ef27335055bc0422
mohamed formation <formation.mohamed.m2i@gmail.com>
2022-12-03 18:34:38 +0100

    first commit

---
+++ src/main/java/net/neosoft/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package net.neosoft.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.neosoft.springboot.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
